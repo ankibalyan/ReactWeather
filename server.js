9db7c991a7c2c3e59093c96945d60265c12cd9a4
Ankit Kumar <ankit@ipapagari.com>
2017-02-11 23:23:41 +0530

    prep for heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 9000;
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+  next();
+})
 app.use(express.static('public'));
 
-app.listen(9000, function () {
-  console.log('Your app is up and running at http://localhost:9000');
+app.listen(PORT, function () {
+  console.log('Your app is up and running at http://localhost:'+PORT);
 });
