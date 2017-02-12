1ecd3f72f8c84d9343728441dcc379b2ac3885e7
Ankit Kumar <ankit@ipapagari.com>
2017-02-12 19:35:05 +0530

    added about page content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Hi! This is Ankit here.</p>
+      <h3 className="text-center">About</h3>
+      <p>Hi! This is a weather app built with reactJs</p>
+      <p>Here are the some tools that I have used in this app</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React JS</a> --a Javascript framework
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open weather map</a> --api to get weather detail.
+        </li>
+      </ul>
     </div>
   );
 };
