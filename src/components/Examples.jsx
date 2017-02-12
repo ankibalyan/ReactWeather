8d7e5266139ce7ebe5b98dc1328015061e8766e9
Ankit Kumar <ankit@ipapagari.com>
2017-02-12 19:25:05 +0530

    add grid system and style

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Examples = () => {
   return (
     <div>
-      <h3>Examples</h3>
+      <h3 className="text-center">Examples</h3>
       <p>Welcome to example page!</p>
+      <ol>
+        <li>
+          <Link to="/?location=new delhi">New Delhi</Link>
+        </li>
+        <li>
+          <Link to="/?location=bangalore">Bangalore</Link>
+        </li>
+      </ol>
     </div>
   );
 }
